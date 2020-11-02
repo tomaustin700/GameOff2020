bd6903cd932c661a6b9901e0afbba354fc75e488
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-02 16:30:54 +0000

    Added scenes

---
+++ GameOff2020/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float oxygenLevel = 100f;
+    public Text oxygenText;
+    public bool inHab;
+    public bool playerDead;
+    public float oxygenSubtractionMultiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateOxygen", 0, 2.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateOxygen()
+    {
+        if (!inHab)
+        {
+            oxygenLevel = oxygenLevel - oxygenSubtractionMultiplier;
+            if (oxygenLevel < 20)
+                oxygenText.color = Color.red;
+            else
+                oxygenText.color = Color.black;
+
+            if (oxygenLevel == 0)
+                playerDead = true;
+        }
+        else
+        {
+            if (oxygenLevel < 100)
+                oxygenLevel = 100;
+        }
+
+        oxygenText.text = "Oxygen: " + oxygenLevel;
+
+    }
+}
