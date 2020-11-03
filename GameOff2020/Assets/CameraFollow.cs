2299d44b6cfa601ba2f4a0f702924613f934c394
Tom Poucher <Tom.Poucher@streets-heaver.com>
2020-11-03 11:05:25 +0000

    added player movement

---
+++ GameOff2020/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject FollowTarget;
+    private Vector3 CameraOffset;
+    public float MaxZoomInRange = 7.0f;
+    public float MaxZoomOutRange = 5f;
+    public float ScrollSpeed = 3f;
+    [SerializeField]
+    private float CameraZoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraOffset = FollowTarget.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraZoom = Mathf.Clamp(CameraZoom += Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed, -MaxZoomOutRange, MaxZoomInRange);
+        transform.position = FollowTarget.transform.position - (new Vector3(CameraOffset.x, CameraOffset.y + CameraZoom, CameraOffset.z));
+        transform.LookAt(FollowTarget.transform);
+    }
+}
---
+++ GameOff2020/Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public float JumpHeight = 10f;
+    public LayerMask groundLayers;
+    public LayerMask CameraRotateLayerMask;
+    private float horizontal;
+    private float vertical;
+    private Rigidbody rigidBody;
+    private bool queueJump = false;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if(Input.GetKey(KeyCode.Space))
+        {
+            queueJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movementVector = (transform.forward * vertical) + (transform.right * horizontal);
+        rigidBody.MovePosition(transform.position + movementVector * Time.deltaTime * Speed);
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            var newPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            transform.LookAt(newPosition);
+        }
+
+        if(queueJump)
+        {
+            queueJump = false;
+            if (rigidBody.velocity.y <= 0 && Physics.CheckSphere(transform.position, 0.5f, groundLayers))
+            {
+                rigidBody.AddForce((Vector3.up * JumpHeight) + rigidBody.velocity, ForceMode.Impulse);
+            }
+        }
+    }
+}
