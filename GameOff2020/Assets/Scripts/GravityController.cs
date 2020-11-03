49c3430a4252aed9ace508e097fc66b1e95e6e33
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-03 10:26:16 +0000

    Added graviryt manager prefab

---
+++ GameOff2020/Assets/Scripts/GravityController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+	
+	public float gravityX;
+	public float gravityY;
+
+	// Use this for initialization
+	void Start()
+	{
+		Physics2D.gravity = new Vector2(gravityX, gravityY);
+	}
+}
