f794dbc3ffc86c1fb696be8603d271d3b8d13e06
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-04 08:58:47 +0000

    Tweaked scripts and scene to get oxygen working

--- GameOff2020/Assets/Scripts/RoomEntryTrigger.cs
+++ GameOff2020/Assets/Scripts/RoomEntryTrigger.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomEntryTrigger : PlayerManager
+public class RoomEntryTrigger : MonoBehaviour
 {
     [SerializeField] private Material inHabMaterial;
     [SerializeField] private Material defaultMaterial;
     // Start is called before the first frame update
+
+    private PlayerManager playerManager;
+
+    private void Awake()
+    {
+        playerManager = GameObject.Find("Player_WithCamera").GetComponent<PlayerManager>();
+    }
+
     void Start()
     {
         
@@ -19,14 +27,14 @@ public class RoomEntryTrigger : PlayerManager
     }
     void OnTriggerExit()
     {
-        inHab = !inHab;
+        playerManager.inHab = !playerManager.inHab;
 
         var wall1 = GameObject.Find("Wall 1").GetComponent<Renderer>();
         var wall2 = GameObject.Find("Wall 2").GetComponent<Renderer>();
         var wall3 = GameObject.Find("Wall 3").GetComponent<Renderer>();
         var wall4 = GameObject.Find("Wall 4").GetComponent<Renderer>();
 
-        if (inHab)
+        if (playerManager.inHab)
         {
             wall1.material = inHabMaterial;
             wall2.material = inHabMaterial;
