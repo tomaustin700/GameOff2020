c1c463729600ab7288a5b19be151dcb298ce7699
Thomas Poucher <tom.poucher@live.co.uk>
2020-11-05 23:56:56 +0000

    enum spaces

--- GameOff2020/Assets/Scripts/Enums/PlayerAnimationState.cs
+++ GameOff2020/Assets/Scripts/Enums/PlayerAnimationState.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 	public enum PlayerAnimationState
 	{
-			Idle = 0,
-			Walking = 1,
-			Jumping = 2,
-			//Landing = 3,
-			Running = 4,
-			Floating = 5
+		Idle = 0,
+		Walking = 1,
+		Jumping = 2,
+		//Landing = 3,
+		Running = 4,
+		Floating = 5
 	}
