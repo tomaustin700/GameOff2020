8b87d59f895cf36cc59f72587911e6d3dd3b26ad
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-06 18:59:32 +0000

    Added moon body and faux gravity for it

---
+++ GameOff2020/Assets/Scripts/GeneratePlanetEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GeneratePlanet))]
+public class GeneratePlanetEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GeneratePlanet myScript = (GeneratePlanet)target;
+        if (GUILayout.Button("Create Planet"))
+        {
+            myScript.CreatePlanet();
+        }
+    }
+}
