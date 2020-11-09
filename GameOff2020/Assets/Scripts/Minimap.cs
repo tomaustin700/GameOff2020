2321e5e378159e9be14e74890ca29920255cee36
TPMartin <tommartin1994@gmail.com>
2020-11-09 21:10:19 +0000

    added minimap

---
+++ GameOff2020/Assets/Scripts/Minimap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
