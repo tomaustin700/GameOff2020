4597f27758f77fee21821ded4856fe5f1412c371
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-13 17:01:54 +0000

    Added invetory classes and added manager to player prefab

---
+++ GameOff2020/Assets/Scripts/InventoryFullException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class InventoryFullException : Exception
+{
+    public InventoryFullException()
+    {
+    }
+
+    public InventoryFullException(string message)
+        : base(message)
+    {
+    }
+
+    public InventoryFullException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+
+    protected InventoryFullException(System.Runtime.Serialization.SerializationInfo serializationInfo, System.Runtime.Serialization.StreamingContext streamingContext)
+    {
+        throw new NotImplementedException();
+    }
+}
