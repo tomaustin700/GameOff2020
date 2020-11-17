0d9329ed00e41620e0f3a14584953baeabf61603
TPMartin <tommartin1994@gmail.com>
2020-11-17 21:02:31 +0000

    fix inHab script to work with hab prefab

---
+++ GameOff2020/Assets/Scripts/InHabScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InHabScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private PlayerManager playerManager;
+    private void LateUpdate()
+    {
+        if (playerManager == null)
+        {
+            playerManager = GameObject.Find("Player_WithCamera(Clone)").GetComponent<PlayerManager>();
+        }
+    }
+    void OnTriggerEnter()
+    {
+        playerManager.inHab = true;
+    }
+    void OnTriggerExit()
+    {
+        playerManager.inHab = false;
+    }
+}
