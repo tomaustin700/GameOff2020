198a43238e8453adba24c0f270181500c2fddd02
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-17 10:40:42 +0000

    Added prefabs for elements and started adding hotbar

--- GameOff2020/Assets/Scripts/IInventoryItem.cs
+++ GameOff2020/Assets/Scripts/IInventoryItem.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public interface IInventoryItem
 {
 
     string name { get; set; }
+    Image image { get; set; }
 }
--- GameOff2020/Assets/Scripts/Mining/Element.cs
+++ GameOff2020/Assets/Scripts/Mining/Element.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Element : InventoryItem, IElement
 {
     public new string name { get; set; }
     public int chance { get; set; }
+    public Image image { get; set; }
 }
