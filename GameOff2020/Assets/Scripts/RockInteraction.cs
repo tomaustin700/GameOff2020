c533121345bcd37bc5303a857a3a498748066c1e
Tom Austin <Tom.Austin@streets-heaver.com>
2020-11-18 14:04:21 +0000

    Got rock pickup working

---
+++ GameOff2020/Assets/Scripts/RockInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockInteraction : MonoBehaviour
+{
+    private System.Random rnd;
+    private bool isRare;
+
+    public void PickUpRock()
+    {
+
+        if (gameObject.name == "Rock4" || gameObject.name == "Rock5")
+        {
+            isRare = true;
+        }
+
+        GameObject.FindGameObjectWithTag("Hotbar").GetComponent<InventoryManager>().AddItem(isRare ? new InventoryItem("RareRock") : new InventoryItem("CommonRock"));
+        Destroy(gameObject);
+
+    }
+
+
+}
