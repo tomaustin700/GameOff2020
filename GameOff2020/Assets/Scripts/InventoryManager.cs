51006f0258942ebf48108431fe285ee9547a48e3
Thomas Poucher <tom.poucher@live.co.uk>
2020-11-21 18:37:58 +0000

    scene save, merge

--- GameOff2020/Assets/Scripts/InventoryManager.cs
+++ GameOff2020/Assets/Scripts/InventoryManager.cs
@@ -132,20 +132,7 @@ public class InventoryManager : MonoBehaviour
         {
             var slotItem = hotbarLocations.First(a => a.hotbarLocation.Contains(selectedSlot.Value.ToString()));
 
-            var item = itemsInInventory.First(q => q.refId == slotItem.itemGuid);
-            // var asset = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/" + item.name + ".prefab", typeof(UnityEngine.Object)) as GameObject;
-            var asset = Resources.Load("Prefabs/" + item.name) as GameObject;
-
-            GetComponentsInChildren<RawImage>().First(q => q.gameObject.name == hotbarLocations.First(a => a.itemGuid == item.refId).hotbarLocation).texture = null;
-            hotbarLocations.Remove(hotbarLocations.First(a => a.itemGuid == item.refId));
-            itemsInInventory.Remove(itemsInInventory.First(a => a.refId == item.refId));
-
-
-            if (player == null)
-                player = GameObject.Find("Player_Astronaut").GetComponentInChildren<Animator>().gameObject;
-
-            var forward = player.transform.position + player.transform.forward;
-            Instantiate(asset, new Vector3(forward.x, player.transform.position.y + 1.5f, forward.z), player.transform.rotation);
+    
 
 
         }
@@ -231,10 +218,12 @@ public class InventoryManager : MonoBehaviour
 
 
         if (player == null)
-            player = GameObject.Find("Player_Astronaut");
+            player = GameObject.Find("Player_Astronaut").GetComponentInChildren<Animator>().gameObject;
 
         var forward = player.transform.position + player.transform.forward;
         if (instantiate)
+        {
             Instantiate(asset, new Vector3(forward.x, player.transform.position.y + 1.5f, forward.z), player.transform.rotation);
+        }
     }
     }
