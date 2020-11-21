f0d7439e297bdad8b7ca0363b25187c0ff568a81
Thomas Poucher <tom.poucher@live.co.uk>
2020-11-21 20:53:14 +0000

    local commit

--- GameOff2020/Assets/Scripts/UseableObjectsManager.cs
+++ GameOff2020/Assets/Scripts/UseableObjectsManager.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UseableObjectsManager : MonoBehaviour
 {
     [SerializeField] private string useableItemTag = "UseableItem";
     Collider[] hitColliders;
     [SerializeField] private GameObject player;
+    public Text InstructionText;
     void Update()
     {
         if (player != null)
@@ -14,6 +16,7 @@ public class UseableObjectsManager : MonoBehaviour
 
 
             hitColliders = Physics.OverlapSphere(transform.position, 3f);
+            string text = string.Empty;
             foreach (Collider col in hitColliders)
             {
                 if (col.gameObject.CompareTag(useableItemTag))
@@ -22,6 +25,8 @@ public class UseableObjectsManager : MonoBehaviour
                     if (col.GetComponent<SolarPanelDust>() != null)
                     {
                         var solarPanel = col.GetComponent<SolarPanelDust>();
+                       
+                        text = "'E' To Remove Dust";
                         if (Input.GetKey(KeyCode.E))
                         {
                             solarPanel.RemoveDust();
@@ -29,7 +34,10 @@ public class UseableObjectsManager : MonoBehaviour
                     }
                     if (col.GetComponent<Storage>() != null)
                     {
+
                         var storage = col.GetComponent<Storage>();
+
+                        text = storage.IsOpen ? text = "'E' To Close Storage" : "'E' To Open Storage";
                         if (Input.GetKeyUp(KeyCode.E))
                         {
                             storage.ToggleInventory();
@@ -38,7 +46,7 @@ public class UseableObjectsManager : MonoBehaviour
                     if (col.GetComponentInParent<RockInteraction>() != null)
                     {
                         var rock = col.GetComponentInParent<RockInteraction>();
-
+                        text = "'E' To Pick Up Rock"; 
                         if (Input.GetKey(KeyCode.E))
                         {
                             rock.PickUpRock();
@@ -47,6 +55,7 @@ public class UseableObjectsManager : MonoBehaviour
 
                 }
             }
+            InstructionText.text = text;
         }
     }
 }
