9a8df412db1cb86068fc989ccf735bf066d885f1
Tom Poucher <Tom.Poucher@streets-heaver.com>
2020-11-24 15:26:44 +0000

    added solar panel to spawn predusted

--- GameOff2020/Assets/Scripts/SolarPanelDust.cs
+++ GameOff2020/Assets/Scripts/SolarPanelDust.cs
@@ -3,7 +3,7 @@
 public class SolarPanelDust : MonoBehaviour
 {
     [SerializeField] private float SolarDustTime;//150f
-    private int solarPanelStage;
+    public int solarPanelStage;
     [SerializeField] private Material stageOne;
     [SerializeField] private Material stageTwo;
     [SerializeField] private Material stageThree;
