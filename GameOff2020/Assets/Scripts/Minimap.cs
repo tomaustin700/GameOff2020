8c2b526e20756d9d7688d77d358d92407ae368ab
TPMartin <tommartin1994@gmail.com>
2020-11-24 17:13:07 +0000

    minimap cam fix

--- GameOff2020/Assets/Scripts/Minimap.cs
+++ GameOff2020/Assets/Scripts/Minimap.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
     public Transform player;
-
+    float camHeightDiff;
     // Start is called before the first frame update
-
+    private void Start()
+    {
+        camHeightDiff = transform.position.y - player.transform.position.y;
+    }
     // Update is called once per frame
     void LateUpdate()
     {
         Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
+        newPosition.y = newPosition.y + camHeightDiff;
         transform.position = newPosition;
     }
 }
