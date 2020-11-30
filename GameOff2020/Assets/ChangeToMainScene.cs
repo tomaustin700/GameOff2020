0c2bc4d68d0b4b7767fee629be8d39554c0ea5e7
Tom Poucher <Tom.Poucher@streets-heaver.com>
2020-11-30 16:10:58 +0000

    added opening cutscene

---
+++ GameOff2020/Assets/ChangeToMainScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class ChangeToMainScene : MonoBehaviour
+{
+    private PlayableDirector director;
+    public double Time;
+    //   private AsyncOperation operation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+        director.Play();
+    }
+    private void Update()
+    {
+        Time = director.time;
+        if (director.time >= 11.00)
+        {
+            SceneManager.LoadScene("MainScene",LoadSceneMode.Single);
+        }
+    }
+   
+}
